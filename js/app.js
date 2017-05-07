53dabda8ce21839f73dbe409d0413020f3681d37
Ryan <ryan@Ryans-iMac.local>
2017-05-08 01:58:05 +0200

    laaste tweaks

--- js/app.js
+++ js/app.js
@@ -25,6 +25,29 @@ $.ajax({
         $('.status').html(status);
 
 
+        // Product information
+
+        $("#product-info").append("<p>Productnummer: "
+        +order.tracking_number+
+        "</p><p>Bezorgdiesnt: "
+        + order.slug +
+        "</p><p>Bezorgsoort: "
+        + order.shipment_type +
+        "</p><p>Afmeting: 20cm x 10cm x 5cm</p><p>Gewicht: "
+        + order.shipment_weight +
+        "</p><p>Afkomstland: "
+        + order.origin_country_iso3 +
+        "</p><p>Aankomstland: "
+        + order.destination_country_iso3 +
+        "</p><p>Status: "
+        + order.tag +
+        "</p><p>Afleverplaats: "
+        + order.signed_by +
+        "</p>");
+
+
+
+
         // Check status
         if (status == 'Delivered') {
             $('.status').css('color', '#2ecc71');
@@ -50,7 +73,7 @@ $.ajax({
                 $('.informatie').css('border-color', '#2ecc71');
             }
             // List out all checkpoints
-            $('#log').append("<li class='col-md-7'><div class='log-info'><h4>Status: " +
+            $('#log').append("<li><div class='log-info'><h4>Status: " +
                 checkpoints[i].message.toLowerCase() +
                 "</h4><p>Datum: " +
                 new Date(checkpoints[i].checkpoint_time).toLocaleString() +
